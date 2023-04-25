5ed46bfbf5bd2cb8e0f832f8fe8f10b07c62b389
TiyaniK <98035695+TiyaniK@users.noreply.github.com>
2023-04-25 10:03:42 +0200

    fix:cors

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:3000",
+    origin: "https://main--dancing-crumble-3403fe.netlify.app",
     allowedHeaders: ["Content-Type", "Authorization"],
     optionsSuccessStatus: 200,
   })
