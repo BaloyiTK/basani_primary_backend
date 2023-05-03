c1d697b1eaebf7be715175dcf5ab8077ceb0ce05
Tiyani Baloyi <tiyanikevinbaloyi@gmail.com>
2023-05-03 07:34:35 +0200

    updates

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:3000",
+    origin: "https://main--dancing-crumble-3403fe.netlify.app",
     allowedHeaders: ["Content-Type", "Authorization"],
     optionsSuccessStatus: 200,
   })
