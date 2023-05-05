01d16c6b13e40b4bc4c6592509da42a3f82c0032
Tiyani Baloyi <tiyanikevinbaloyi@gmail.com>
2023-05-05 13:52:43 +0200

    updates

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:3000",
+    origin: "https://www.basaniprimary.co.za",
     allowedHeaders: ["Content-Type", "Authorization"],
     optionsSuccessStatus: 200,
   })
